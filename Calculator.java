15f00a05bcdd0bb9256bd7ee797688f256573f44
Jon Bjorn Njalsson <jonn12@ru.is>
2015-10-11 13:37:19 +0000

    refactor some code

--- Calculator.java
+++ Calculator.java
@@ -1,25 +1,33 @@
 
 public class Calculator {
+	
+	private boolean isEmpty(String string) {
+		return string.isEmpty();
+	}
+	
+	private int StringToInt(String string) {
+		return Integer.parseInt(string);
+	}
 
 	public int add(String string) {
-		if(string.isEmpty())
+		if(isEmpty(string))
 		{
 			return 0;
 		}
-		else if (!string.isEmpty())
+		else if (!isEmpty(string))
 		{
 			if(string.length() == 1)
 			{
-				return Integer.parseInt(string);
+				return StringToInt(string);
 			}
 			else
 			{
 				String[] arr = string.split(",");
-				return (Integer.parseInt(arr[0]) + Integer.parseInt(arr[1]));
+				return (StringToInt(arr[0]) + StringToInt(arr[1]));
 			}
 			
 		}
-		return Integer.parseInt(string);
+		return StringToInt(string);
 	}
 
 }
